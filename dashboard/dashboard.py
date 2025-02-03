fb7171d4b7ea460817341e5d7bd6a181793cc6df
kresnaj <kusmonoj@gmail.com>
2025-02-03 11:38:43 +0700

    Perbaikan kode

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,40 +5,13 @@ import pandas as pd
 
 sl.title('Hasil Analisis Kualitas Udara Statiun Nongzhanguan, China.')
 sl.write('Data ini diambil dari [Github](https://github.com/marceloreis/HTI/tree/master)')
-polutan_utama, polutan_lainnya, korelasi_kecepatan_angin = sl.tabs(['Polutan Utama', 'Polutan Lainnya', 'Korelasi Kecepatan Angin'])
+tren_polutan, korelasi_kecepatan_angin = sl.tabs(['Tren ', 'Korelasi Kecepatan Angin'])
 
-with polutan_utama:
-    sl.header('Tren 2013-2017')
-    air_quality_nongzhanguan_df = pd.read_csv('main_data.csv')
-    tren_air_quality = air_quality_nongzhanguan_df.groupby(by=['year', 'month']).agg({
-        'PM2.5': 'mean',
-        'PM10': 'mean',
-        'SO2': 'mean',
-        'NO2': 'mean',
-        'CO': 'mean',
-        'O3': 'mean',
-        'TEMP': 'mean',
-        'PRES': 'mean',
-        'DEWP': 'mean',
-        'RAIN': 'mean',
-        'WSPM': 'mean'
-    }).apply(lambda c: c.astype(int))
-
-        # Code 1
-    main_pollutants = ['PM2.5', 'PM10', 'SO2', 'NO2', 'CO', 'O3']
-    fig = plt.figure(figsize=(8, 6))
-    for pollutant in main_pollutants:
-        sb.lineplot(tren_air_quality, x='year', y=pollutant, label=pollutant)
-    plt.xticks(rotation=50)
-    plt.legend(loc='upper right')
-    plt.xlabel('Year - Month')
-    plt.ylabel('Micrometer/meter^3')
-    plt.grid(True)
-    sl.pyplot(fig)
-    with sl.expander("See code"):
-        code = """
+with tren_polutan:
+    main_pollutants, other_pollutants = sl.columns(2)
+    with main_pollutants:
+        sl.header('Polutan Utama')
         air_quality_nongzhanguan_df = pd.read_csv('main_data.csv')
-
         tren_air_quality = air_quality_nongzhanguan_df.groupby(by=['year', 'month']).agg({
             'PM2.5': 'mean',
             'PM10': 'mean',
@@ -53,9 +26,9 @@ with polutan_utama:
             'WSPM': 'mean'
         }).apply(lambda c: c.astype(int))
 
+        # Code 1
         main_pollutants = ['PM2.5', 'PM10', 'SO2', 'NO2', 'CO', 'O3']
-
-        plt.figure(figsize=(8, 6))
+        fig = plt.figure(figsize=(8, 6))
         for pollutant in main_pollutants:
             sb.lineplot(tren_air_quality, x='year', y=pollutant, label=pollutant)
         plt.xticks(rotation=50)
@@ -63,25 +36,41 @@ with polutan_utama:
         plt.xlabel('Year - Month')
         plt.ylabel('Micrometer/meter^3')
         plt.grid(True)
-        plt.show()"""
-        sl.code(code, language='python')
+        sl.pyplot(fig)
+        with sl.expander("See code"):
+            code = """
+            air_quality_nongzhanguan_df = pd.read_csv('main_data.csv')
 
-with polutan_lainnya:
-    sl.header('Tren 2013-2017')
-    other_pollutants = ['TEMP', 'PRES', 'DEWP', 'RAIN', 'WSPM']
-    fig = plt.figure(figsize=(8, 6))
-    for pollutant in other_pollutants:
-        sb.lineplot(tren_air_quality, x='year', y=pollutant, label=pollutant)
-    plt.xticks(rotation=50)
-    plt.legend(loc='upper right')
-    plt.xlabel('Year - Month')
-    plt.ylabel('Micrometer/meter^3')
-    plt.grid(True)
-    sl.pyplot(fig)
-    with sl.expander("See code"):
-        code = """
+            tren_air_quality = air_quality_nongzhanguan_df.groupby(by=['year', 'month']).agg({
+                'PM2.5': 'mean',
+                'PM10': 'mean',
+                'SO2': 'mean',
+                'NO2': 'mean',
+                'CO': 'mean',
+                'O3': 'mean',
+                'TEMP': 'mean',
+                'PRES': 'mean',
+                'DEWP': 'mean',
+                'RAIN': 'mean',
+                'WSPM': 'mean'
+            }).apply(lambda c: c.astype(int))
+
+            main_pollutants = ['PM2.5', 'PM10', 'SO2', 'NO2', 'CO', 'O3']
+
+            plt.figure(figsize=(8, 6))
+            for pollutant in main_pollutants:
+            sb.lineplot(tren_air_quality, x='year', y=pollutant, label=pollutant)
+            plt.xticks(rotation=50)
+            plt.legend(loc='upper right')
+            plt.xlabel('Year - Month')
+            plt.ylabel('Micrometer/meter^3')
+            plt.grid(True)
+            plt.show()"""
+            sl.code(code, language='python')
+
+    with other_pollutants:
+        sl.header("Polutan Lain")
         air_quality_nongzhanguan_df = pd.read_csv('main_data.csv')
-
         tren_air_quality = air_quality_nongzhanguan_df.groupby(by=['year', 'month']).agg({
             'PM2.5': 'mean',
             'PM10': 'mean',
@@ -95,10 +84,8 @@ with polutan_lainnya:
             'RAIN': 'mean',
             'WSPM': 'mean'
         }).apply(lambda c: c.astype(int))
-        
         other_pollutants = ['TEMP', 'PRES', 'DEWP', 'RAIN', 'WSPM']
-
-        plt.figure(figsize=(8, 6))
+        fig = plt.figure(figsize=(8, 6))
         for pollutant in other_pollutants:
             sb.lineplot(tren_air_quality, x='year', y=pollutant, label=pollutant)
         plt.xticks(rotation=50)
@@ -106,8 +93,38 @@ with polutan_lainnya:
         plt.xlabel('Year - Month')
         plt.ylabel('Micrometer/meter^3')
         plt.grid(True)
-        plt.show()"""
-        sl.code(code, language='python')
+        sl.pyplot(fig)
+
+        with sl.expander("See code"):
+            code = """
+            air_quality_nongzhanguan_df = pd.read_csv('main_data.csv')
+
+            tren_air_quality = air_quality_nongzhanguan_df.groupby(by=['year', 'month']).agg({
+                'PM2.5': 'mean',
+                'PM10': 'mean',
+                'SO2': 'mean',
+                'NO2': 'mean',
+                'CO': 'mean',
+                'O3': 'mean',
+                'TEMP': 'mean',
+                'PRES': 'mean',
+                'DEWP': 'mean',
+                'RAIN': 'mean',
+                'WSPM': 'mean'
+            }).apply(lambda c: c.astype(int))
+        
+            other_pollutants = ['TEMP', 'PRES', 'DEWP', 'RAIN', 'WSPM']
+
+            plt.figure(figsize=(8, 6))
+            for pollutant in other_pollutants:
+                sb.lineplot(tren_air_quality, x='year', y=pollutant, label=pollutant)
+            plt.xticks(rotation=50)
+            plt.legend(loc='upper right')
+            plt.xlabel('Year - Month')
+            plt.ylabel('Micrometer/meter^3')
+            plt.grid(True)
+            plt.show()"""
+            sl.code(code, language='python')
 
 with korelasi_kecepatan_angin:
     sl.header('Korelasi kecepatan angin dengan Polutan Utama')
